4d48fcd3e1155d211e9b6d7b3aaf34b016d70fc0
Max <max.eboch@magna.com>
2018-06-14 15:21:55 -0400

    Removed directories not needed

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/TestWS/src
-# Build directory: /home/max/TestWS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(agitr)
--- build/agitr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/TestWS/src/agitr
-# Build directory: /home/max/TestWS/build/agitr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
